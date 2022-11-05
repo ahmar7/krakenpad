1fe03e3b879532b52f60f20479f134c9fd16cba0
A. Developers <ahmarjabbar7@gmail.com>
2022-11-06 02:47:18 +0500

    final

--- src/utils/allImg.js
+++ src/utils/allImg.js
@@ -6,5 +6,7 @@ import Four from "../assets/img/4.png";
 import Five from "../assets/img/5.png";
 import Six from "../assets/img/6.png";
 import Banner from "../assets/img/banner.png";
+import Ico from "../assets/img/ico.png";
+import Dp from "../assets/img/dp.png";
 
-export { Logo, One, Two, Three, Four, Five, Six, Banner };
+export { Logo, One, Two, Three, Four, Five, Six, Banner, Ico, Dp };
